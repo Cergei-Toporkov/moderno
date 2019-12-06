39f5168f54dc2666381a7742ad9a317462e89a0f
Cergei-Toporkov <Cergei-Toporkov@rambler.ru>
2019-12-06 12:24:37 +0500

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,25 @@
 $(function() {
-  $(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    readOnly: true
-  });
+ $(".rate-star").rateYo({
+  rating: 5,
+  starWidth: "12px",
+  readOnly: true,
+ });
 
-  $(".product-slider__inner").slick({
-    dots: true,
-    arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 4
-  });
+ $(".product-slider__inner").slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+ });
 
-  var mixer = mixitup(".products__inner-box");
+ $(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 0,
+  to: 600,
+  prefix: "$",
+ });
+
+ var mixer = mixitup(".products__inner-box");
 });
